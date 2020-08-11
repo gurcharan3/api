735ab71880aed843d04c714170774e1482a0b9ae
channi <gurcharangujjar3515@gmail.com>
2020-08-11 21:06:04 +0530

    Updates

---
+++ precache-manifest.e9467d834f7f5c41bb8f5f6541018ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12703575e19cbf615c5664fa56e38ee1",
+    "url": "/api/index.html"
+  },
+  {
+    "revision": "34de39f268d89dbdd077",
+    "url": "/api/static/css/main.17626692.chunk.css"
+  },
+  {
+    "revision": "03f49c3208f1fed81e4b",
+    "url": "/api/static/js/2.d021ec35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/api/static/js/2.d021ec35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34de39f268d89dbdd077",
+    "url": "/api/static/js/main.ed4bc023.chunk.js"
+  },
+  {
+    "revision": "41b0debc7491b6b2d364",
+    "url": "/api/static/js/runtime-main.b61933af.js"
+  }
+]);
